e1fd300c9861e1545d862f1e6a3cb1a6baa05d84
Adam Erstad <adamerstad@gmail.com>
2024-02-20 04:08:24 -0800

    Exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(input, num) {
+    if (num < 0) { return 'ERROR';}
+    let text = '';
+    if (num > 0)
+    {
+        for (let i = 0; i < num; i++)
+        {
+            text += input;
+        }
+    }
+    return text;
 };
 
 // Do not edit below this line
