f3126dd8b9ba014c41a7ee668e365787f244694b
Adam Erstad <adamerstad@gmail.com>
2024-02-20 22:45:03 -0800

    Exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...removals) {
+    let newArray = [];
+    for (element of inputArray)
+    {
+        if(!removals.find(x => x === element))
+        {
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
