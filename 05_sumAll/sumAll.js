b4d52eb8048e9f2f06e300545fb2e5cd55dcc887
Adam Erstad <adamerstad@gmail.com>
2024-02-20 22:59:47 -0800

    Exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1 + num2) || num1 < 0 || num2 < 0) { return 'ERROR'; }
+    let sum = 0;
+    let smallerNumber = num1 < num2 ? num1 : num2;
+    let largerNumber = num1 > num2 ? num1 : num2;
+    for (let i = smallerNumber; i <= largerNumber; i++) { sum += i; }
+    return sum;
 };
 
 // Do not edit below this line
