acab8d07198b633255507304d79f5105f4101462
Adam Erstad <adamerstad@gmail.com>
2024-02-26 04:39:09 -0800

    Exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    return input.sort((a, b) => {
+        if (a.yearOfDeath === undefined) { a.yearOfDeath = 2023; }
+        if (b.yearOfDeath === undefined) { b.yearOfDeath = 2023; }
+        let aOldest = a.yearOfDeath - a.yearOfBirth;
+        let bOldest = b.yearOfDeath - b.yearOfBirth;
+        return aOldest > bOldest ? -1 : 1;
+    })[0];
 };
 
 // Do not edit below this line
