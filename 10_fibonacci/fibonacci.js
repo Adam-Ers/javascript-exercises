bc0f0595d064bbfd93d6f5fb0563340d4cf1125c
Adam Erstad <adamerstad@gmail.com>
2024-02-26 04:18:21 -0800

    Exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    input = parseInt(input);
+    if (input < 0) { return "OOPS"; }
+    if (input < 2) { return input; }
+    if (input >= 2)
+    {
+        return fibonacci(input - 1) + fibonacci(input - 2);
+    }
 };
 
 // Do not edit below this line
