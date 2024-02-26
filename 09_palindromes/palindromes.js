cb8fe84e59e435bb48d18511e38cdcdbe109c20d
Adam Erstad <adamerstad@gmail.com>
2024-02-26 03:21:50 -0800

    Exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    /** @type {String} **/
+    let stringy = s;
 
+    stringy = stringy.toLowerCase();
+    stringy = stringy.replace(/[,]|[.]|[ ]|[!]|[?]/gm, '');
+    let reverseStringy = '';
+    for (i = stringy.length - 1; i >= 0; i--)
+    {
+        reverseStringy += stringy.charAt(i);
+    }
+    console.log(reverseStringy);
+
+    return stringy === reverseStringy;
 };
 
 // Do not edit below this line
