4be1bb74ba082ec0b2a695b33172300d6ebbaa88
Adam Erstad <adamerstad@gmail.com>
2024-02-26 03:01:18 -0800

    Exercise 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    return parseInt(nums.reduce((total, num) => total * num, 1));
 };
 
-const power = function() {
-	
+const power = function(a, pow) {
+    let originalA = a;
+    for (i = 1; i < pow; i++)
+    {
+        a *= originalA;
+    }
+
+    return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let num = 1;
+    for (i = 1; i < a; i++)
+    {
+        num *= i + 1;
+    }
+    return num;
 };
 
 // Do not edit below this line
